445f37427703addb67ad910803aa8d3ae904adc3
artemmatiushenko1 <artom.matyushenko@gmail.com>
2023-11-21 21:14:09 +0200

    feat: + jwt token auth app

---
+++ lab-1/jwt_token_auth/index.js
@@ -0,0 +1,102 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+
+const path = require('path');
+
+const PORT = 3000;
+const JWT_SECRET_KEY = 'JWT_SECRET_KEY';
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const SESSION_KEY = 'Authorization';
+
+app.use((req, res, next) => {
+  const authToken = req.get(SESSION_KEY);
+
+  if (!authToken) {
+    return next();
+  }
+
+  try {
+    const { login } = jwt.verify(authToken, JWT_SECRET_KEY);
+
+    if (!login) {
+      return res.status(401).send();
+    }
+
+    const user = users.find((user) => user.login === login);
+
+    if (!user) {
+      return res.status(401).send();
+    }
+
+    req.user = user;
+  } catch {
+    return res.status(401).send();
+  }
+
+  return next();
+});
+
+app.get('/', (req, res) => {
+  if (req.user) {
+    return res.json({
+      username: req.user.username,
+      logout: 'http://localhost:3000/logout',
+    });
+  }
+
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/logout', (req, res) => {
+  res.redirect('/');
+});
+
+const users = [
+  {
+    login: 'Login',
+    password: 'Password',
+    username: 'Username',
+  },
+  {
+    login: 'Login1',
+    password: 'Password1',
+    username: 'Username1',
+  },
+  {
+    login: 'artom.matyushenko@gmail.com',
+    password: '12345678aA@',
+    username: 'Artem',
+  },
+];
+
+app.post('/api/login', (req, res) => {
+  const { login, password } = req.body;
+
+  const user = users.find((user) => {
+    if (user.login === login && user.password === password) {
+      return true;
+    }
+    return false;
+  });
+
+  if (user) {
+    const token = jwt.sign(
+      { username: user.username, login: user.login },
+      JWT_SECRET_KEY,
+      { expiresIn: '10s' }
+    );
+
+    return res.json({ token });
+  }
+
+  return res.status(401).send();
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
