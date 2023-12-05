72b413a90a068cc94b4365cabfb4228937b83db5
artemmatiushenko1 <artom.matyushenko@gmail.com>
2023-12-05 14:53:25 +0200

    feat: + auth0 sign in

--- lab-4/index.js
+++ lab-4/index.js
@@ -1,40 +1,77 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
-
+const jwksClient = require('jwks-rsa');
 const path = require('path');
-
-const PORT = 3000;
-const JWT_SECRET_KEY = 'JWT_SECRET_KEY';
+const { AuthenticationClient, ManagementClient } = require('auth0');
 
 const app = express();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const SESSION_KEY = 'Authorization';
 
-app.use((req, res, next) => {
-  const authToken = req.get(SESSION_KEY);
+const auth = new AuthenticationClient({
+  domain: process.env.DOMAIN,
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+});
+
+const management = new ManagementClient({
+  domain: process.env.DOMAIN,
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+});
+
+const verifyToken = async (bearerToken) => {
+  const jwks = jwksClient({
+    jwksUri: `https://${process.env.DOMAIN}/.well-known/jwks.json`,
+  });
+
+  const getJwksClientKey = (header, callback) => {
+    jwks.getSigningKey(header.kid, (_, key) => {
+      const signingKey = key.publicKey || key.rsaPublicKey;
+      callback(null, signingKey);
+    });
+  };
+
+  return new Promise((resolve, reject) => {
+    jwt.verify(
+      bearerToken,
+      getJwksClientKey,
+      {
+        audience: process.env.AUDIENCE,
+        issuer: `https://${process.env.DOMAIN}/`,
+        algorithms: ['RS256'],
+      },
+      (err, decoded) => (err ? reject(err) : resolve(decoded))
+    );
+  });
+};
+
+app.use(async (req, res, next) => {
+  const authToken = req.get(SESSION_KEY)?.split(' ').pop();
 
   if (!authToken) {
     return next();
   }
 
   try {
-    const { login } = jwt.verify(authToken, JWT_SECRET_KEY);
+    const { sub: userId } = (await verifyToken(authToken)) ?? {};
 
-    if (!login) {
+    if (!userId) {
       return res.status(401).send();
     }
 
-    const user = users.find((user) => user.login === login);
+    const { data: user } = await management.users.get({ id: userId });
 
     if (!user) {
       return res.status(401).send();
     }
 
     req.user = user;
-  } catch {
+  } catch (err) {
     return res.status(401).send();
   }
 
@@ -44,8 +81,8 @@ app.use((req, res, next) => {
 app.get('/', (req, res) => {
   if (req.user) {
     return res.json({
-      username: req.user.username,
-      logout: 'http://localhost:3000/logout',
+      username: req.user.name,
+      logout: `http://localhost:${process.env.APP_PORT}/logout`,
     });
   }
 
@@ -56,47 +93,26 @@ app.get('/logout', (req, res) => {
   res.redirect('/');
 });
 
-const users = [
-  {
-    login: 'Login',
-    password: 'Password',
-    username: 'Username',
-  },
-  {
-    login: 'Login1',
-    password: 'Password1',
-    username: 'Username1',
-  },
-  {
-    login: 'artom.matyushenko@gmail.com',
-    password: '12345678aA@',
-    username: 'Artem',
-  },
-];
-
-app.post('/api/login', (req, res) => {
+app.post('/api/login', async (req, res) => {
   const { login, password } = req.body;
 
-  const user = users.find((user) => {
-    if (user.login === login && user.password === password) {
-      return true;
-    }
-    return false;
-  });
+  try {
+    const { data: tokens } = await auth.oauth.passwordGrant({
+      password,
+      username: login,
+      audience: process.env.AUDIENCE,
+      scope: 'offline_access',
+    });
 
-  if (user) {
-    const token = jwt.sign(
-      { username: user.username, login: user.login },
-      JWT_SECRET_KEY,
-      { expiresIn: '6h' }
-    );
-
-    return res.json({ token });
+    return res.json({
+      token: tokens.access_token,
+      refreshToken: tokens.refresh_token,
+    });
+  } catch {
+    return res.status(401).send();
   }
-
-  return res.status(401).send();
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
+app.listen(process.env.APP_PORT, () => {
+  console.log(`Example app listening on port ${process.env.APP_PORT}`);
 });
